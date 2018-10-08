54660cb0ae675b401a9968f602d92bfcaa9a9bcd
peinilla <peinilla1569@gmail.com>
2018-10-09 01:08:18 +0900

    화살표 북쪽

--- CauNavi_proto/app/src/main/java/com/example/hh/caunavi_proto/GpsManager.java
+++ CauNavi_proto/app/src/main/java/com/example/hh/caunavi_proto/GpsManager.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -49,6 +50,8 @@ public class GpsManager extends Service implements LocationListener {
         getLocation();
         gpsText = t;
     }
+
+
 
     @TargetApi(23)
     public Location getLocation() {
